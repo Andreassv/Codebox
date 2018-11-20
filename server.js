42d153f7fcfd81829e867688b9247a9014b58331
Andreassv <35996622+Andreassv@users.noreply.github.com>
2018-11-20 13:24:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require('express')();
-
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const logger = require('morgan');
+const bcrypt = require('bcrypt');
+const fs = require('fs');
 require('./modules/views/index')(app);
 
 app.listen(3000);
