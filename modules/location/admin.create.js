ff95fcd1e225986b9c6f7bbc00fabf92ed89f903
Andreassv <35996622+Andreassv@users.noreply.github.com>
2019-02-26 08:33:00 +0100

    Prøver at lave crud...

--- modules/location/admin.create.js
+++ modules/location/admin.create.js
@@ -1,31 +1,46 @@
 const db = require("../../config/mysql")();
 
 module.exports = function (app) {
-    app.get('/admin/create', (req, res, next) => {
-        res.render('admin/admin.create.ejs');
+    app.get('/admin/create-user', (req, res, next) => {
+        db.query('SELECT id, list FROM tableformlists', (error, results) => {
+        res.render('admin/admin.create.ejs', {results});
+        })
     });
-    app.post('/create', (req, res, next) => {
-        let success = true;
-        let errorMessage;
-        if (!req.fields.username || !req.fields.username.lenght > 1) {
-            success = false;
-            errorMessage = 'Dit bugernavn må kun indeholde store og små bogstaver';
-        }
-        if (!req.fields.passphrase || !req.fields.passphrase.lenght > 3) {
-            success = false;
-            errorMessage = 'Dit kodeord skal være på minimum 4 tegn';
-        }
-        if (success) {
-            let message;
-            db.query('INSERT INTO users SET username = ?, passphrase = ?, roles_id = (SELECT id from roles WHERE level = 10)', [req.fields.username, req.fields.passphrase],
-                (error, result) => { 
-                if (error) {
-                    message = `noget gik galt: ${error}`;
-                } else {
-                    message = 'Tak for din tilmelding';
-                }
-                res.redirect('/login');
+    app.post('/create-user', (req, res, next) => {
+        if (!req.fields || !req.fields.username || !req.fields.passphrase)
+            throw new Error('Bad request');
+        
+        // const hash = bcrypt.hashSync(req.fields.passphrase, 10);
+    
+        db.execute('INSERT INTO users SET username = ?, passphrase = ?, roles_id = (SELECT id from roles WHERE level = 10)',
+            [req.fields.username], (err, rows) => {
+                if (err) throw new Error(err);
+                res.redirect(`/user/${rows.insertId}`);
             });
-        }
     });
+    // app.post('/create-user', (req, res, next) => {
+    //     let success = true;
+    //     let errorMessage;
+    //     if (req.fields.subject = 'Character') {
+
+    //         success = false;
+    //         errorMessage = 'Dit bugernavn må kun indeholde store og små bogstaver';
+    //     }
+    //     if (!req.fields.passphrase || !req.fields.passphrase.lenght > 3) {
+    //         success = false;
+    //         errorMessage = 'Dit kodeord skal være på minimum 4 tegn';
+    //     }
+    //     if (success) {
+    //         let message;
+    //         db.query('INSERT INTO users SET username = ?, passphrase = ?, roles_id = (SELECT id from roles WHERE level = 10)', [req.fields.username, req.fields.passphrase],
+    //             (error, result) => { 
+    //             if (error) {
+    //                 message = `noget gik galt: ${error}`;
+    //             } else {
+    //                 message = 'Tak for din tilmelding';
+    //             }
+    //             res.redirect('/login');
+    //         });
+    //     }
+    // });
 }
